8887bc60b641cd6247149a335eedbbb5c68d75db
Américo Vitor <americovitor1234@gmail.com>
2025-07-03 00:16:52 -0300

    Add files via upload

---
+++ Vestuario/src/br/main/banco/itens/IEmprestavel.java
@@ -0,0 +1,5 @@
+package br.main.banco.itens;
+
+public interface IEmprestavel {
+	
+}
---
+++ Vestuario/src/br/main/banco/itens/Roupa.java
@@ -0,0 +1,9 @@
+package br.main.banco.itens;
+
+public abstract class Roupa extends Item {
+	public Roupa(String tipo, String Loja_Origem, String Cor, String Conservacao) {
+		super(tipo, Loja_Origem, Cor, Conservacao);
+	}
+	
+	
+}
