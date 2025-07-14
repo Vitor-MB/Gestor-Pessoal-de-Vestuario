31d8275d3a8a0eaf9474279f717a01d80364512d
Américo Vitor <americovitor1234@gmail.com>
2025-07-14 02:47:12 -0300

    Delete Vestuario directory

--- Vestuario/src/br/main/banco/itens/Camisa.java
+++
@@ -1,8 +0,0 @@
-package br.main.banco.itens;
-
-public class Camisa extends Roupa{
-	public Camisa(String tipo, String Loja_Origem, String Cor, String Conservacao, int vezes_usado) {
-		super(tipo, Loja_Origem, Cor, Conservacao, vezes_usado);
-		
-	}
-}
